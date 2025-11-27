fc7ae5e8af7fe38c1463deb1dc11697d68cbdab1
Angelina <110882097+byangelina@users.noreply.github.com>
2025-11-27 18:47:14 -0300

    Add JavaScript variables and console logging examples

---
+++ Parte 1- 2 - 3/01.js
@@ -0,0 +1,23 @@
+
+// alert("hola mundo con JavaScript")
+// esto imprime una ventana emergente con ese mensaje
+
+
+// ahora le vamos a agregar algo más al alert:
+var nombre = "Angela";
+var edad = 25;
+var presenteONo = true;
+var apellido = "Urrutia" ;
+var estatura = 1.70;
+
+// console.log(" ") es para imprimir
+console.log ("Nombre: "+ nombre + " " + typeof nombre); // typeof es para conocer el tipo de dato.
+                                                        // esto se puede ver en el inspector de consola:  Nombre: Angela string
+console.log ("Edad: "+ edad + " " + typeof edad);
+console.log ("Pesente o no: "+ presenteONo + " " + typeof presenteONo);
+console.log ("Apellido: "+ apellido + " " + typeof apellido);
+console.log ("Estatura: "+ estatura + " " + typeof estatura);
+// concatenación: 
+// alert("hola mundo con JavaScript " + nombre);
+
+
