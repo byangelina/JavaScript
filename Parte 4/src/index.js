be2de34134c304e7bfc83e3a751de492cdf5d455
Angelina <110882097+byangelina@users.noreply.github.com>
2025-11-27 18:51:32 -0300

    Add examples of variable scope in JavaScript

---
+++ Parte 4/src/index.js
@@ -0,0 +1,24 @@
+
+
+// Ejemplo 1 ------------------------------------------------------------------------
+/*
+if (true) {
+var nombre = "Suscribete";
+console.log(nombre);
+}*/
+
+
+
+
+// Ejemplo 2 ------------------------------------------------------------------------
+/*
+if (true){
+    let nombre = "Suscribete";
+}
+
+console.log(nombre);
+*/
+/* esto no se puede hacer, da error
+porque no existe "nombre" afuera, se encuentra adentro del bloque if solamente,
+esto ocurre porque es un let, un tipo de dato que solo funciona en un lugar especifico, no en todas partes
+*/
